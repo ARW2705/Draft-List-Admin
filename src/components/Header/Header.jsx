c90463f6239e8452ba45ff930b9f83017871d67d
Andrew Wanex <awanex2705@gmail.com>
2022-05-13 15:36:17 -0700

    feature: add smart header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Navbar from '../Navbar/Navbar'
+
+import throttle from '../../shared/utilities/throttle'
 
 import './Header.css'
 
 
 function Header() {
+  const [ scrollClass, setScrollClass ] = useState({ className: '' })
+
+  let previousScrollPosition
+  const handleScrollEvent = () => {
+    console.log('scroll event')
+    const currentScrollPosition = window.scrollY || window.pageYOffset
+    if (currentScrollPosition > previousScrollPosition) {
+      setScrollClass({ className: 'scroll-down' })
+    } else if (currentScrollPosition < previousScrollPosition) {
+      setScrollClass({ className: 'scroll-up' })
+    }
+    previousScrollPosition = currentScrollPosition
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', throttle(handleScrollEvent, 250))
+    return () => window.removeEventListener('scroll', throttle)
+  }, [])
+
   return (
-    <header className="Header">
+    <header className={`Header ${scrollClass.className}`}>
       <h1>Draft List</h1>
       <Navbar />
     </header>
