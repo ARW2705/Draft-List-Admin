7e899c7ef3bda0867673185e4d6fc9237579e9ac
Andrew Wanex <awanex2705@gmail.com>
2022-05-13 15:37:51 -0700

    feature: add navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,28 @@ import './Navbar.css'
 
 
 function Navbar() {
+  const onNavClick = event => {
+    console.log('nav click event', event)
+  }
+
+  const pageNames = [ 'Manage', 'Beverages', 'User' ]
+  const links = pageNames.map(pageName => {
+    const name = pageName.toLowerCase()
+    return (
+      <Link
+        key={ name }
+        to={ `/${name}` }
+        aria-labelledby={ `nav-${name}` }
+        onClick={ onNavClick }
+      >
+        <span id={ `nav-${name}` }>{ pageName }</span>
+      </Link>
+    )
+  })
+
   return (
     <nav className="Navbar">
-      <Link to='/manage'>Manage</Link>
-      <Link to='/beverages'>Beverages</Link>
-      <Link to='/user'>User</Link>
+      { links }
     </nav>
   )
 }
