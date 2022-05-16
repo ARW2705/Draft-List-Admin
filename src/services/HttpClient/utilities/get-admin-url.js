b497a270c00eabfc4a6c705664d7cbd09580b83f
Andrew Wanex <awanex2705@gmail.com>
2022-05-16 12:56:10 -0700

    refactor: update http client to handle multiple routes

--- src/services/HttpClient/utilities/get-admin-url.js
+++
@@ -1,7 +0,0 @@
-import { BASE_URL } from './base-url'
-
-const getClientURL = endpoint => {
-  return `${BASE_URL}/devices/admin/${endpoint}`
-}
-
-export default getClientURL
--- src/services/HttpClient/utilities/get-image-url.js
+++
@@ -1,7 +0,0 @@
-import { BASE_URL } from './base-url.js'
-
-const getImageURL = endpoint => {
-  return `${BASE_URL}/images/client/${endpoint}`
-}
-
-export default getImageURL
