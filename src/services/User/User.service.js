406f996526435b29a3640c4f49cb13f71f636a98
Andrew Wanex <awanex2705@gmail.com>
2022-05-16 12:55:00 -0700

    feature: add base user service

---
+++ src/services/User/User.service.js
@@ -0,0 +1,53 @@
+import { BehaviorSubject } from 'rxjs'
+
+import httpClient from '../HttpClient/HttpClient.service'
+
+
+class User {
+  constructor() {
+    if (User._instance) return User._instance
+    User._instance = this
+    this.storageKey = 'user'
+    this.user$ = new BehaviorSubject(null)
+  }
+
+  getUser() {
+    return this.user$
+  }
+
+  setUser(user) {
+    this.user$.next(user)
+  }
+
+  init() {
+    if (this._isInitializing) return
+    this._isInitializing = true
+    const user = JSON.parse(localStorage.getItem(this.storageKey))
+    console.log('got user from storage', user)
+    this.user$.next(user)
+  }
+
+  login(credentials) {
+    httpClient.postUser('login', credentials)
+      .then(user => this.setUser(user))
+      .catch(error => {
+        console.error('error on login', error)
+        // TODO handle user feedback
+      })
+  }
+
+  signup(userData) {
+    httpClient.postUser('signup', userData)
+      .then(user => this.setUser(user))
+      .catch(error => {
+        console.error('error on signup', error)
+        // TODO handle user feedback
+      })
+  }
+
+  storeUser(user) {
+    localStorage.setItem(this.storageKey, JSON.stringify(user))
+  }
+}
+
+export default User
