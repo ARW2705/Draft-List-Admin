b6a1c9dc1437a4f4d35080ee48bfa92f0a9c9ac8
Andrew Wanex <awanex2705@gmail.com>
2022-06-02 12:50:42 -0700

    feature: add login form component

---
+++ src/components/Forms/Login/Login.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import userService from '../../../services/User/User.service'
+
+import FormGroup from '../../Common/Form/FormGroup/FormGroup'
+
+import createForm from '../../../shared/form/create-form'
+import { required } from '../../../shared/validators/validators'
+
+
+function Login() {
+  const form = createForm({
+    fields: {
+      username: {
+        validators: [required()]
+      },
+      password: {
+        validators: [required()],
+        options: {
+          type: 'password'
+        }
+      },
+      remember: {
+        element: 'checkbox',
+        value: false,
+        options: {
+          label: 'remember me'
+        }
+      },
+      showPassword: {
+        element: 'checkbox',
+        value: false,
+        options: {
+          label: 'show password'
+        }
+      }
+    },
+    onChanges: {
+      showPassword: {
+        password: {
+          type: show => show ? 'text' : 'password'
+        }
+      }
+    }
+  })
+
+  const handleSubmit = ({ username, password, remember }) => {
+    userService.login({ username, password, remember })
+      .then(() => console.log('login complete'))
+      .catch(error => {
+        // TODO handle error feedback
+      })
+  }
+
+  return (
+    <FormGroup
+      form={ form }
+      submitHandler={ handleSubmit }
+      customClass='login'
+    />
+  )
+}
+
+export default Login
