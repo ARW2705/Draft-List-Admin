18bfd74694b1514786939c1f49fd07b57ee1da9d
Andrew Wanex <awanex2705@gmail.com>
2022-06-01 17:44:33 -0700

    refactor: inital setup for conditional routing of user page based on user status

--- src/routing/routes/User/User.jsx
+++ src/routing/routes/User/User.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import userService from '../../../services/User/User.service'
+
+import SignupForm from '../../../components/Forms/Signup/Signup'
 
 
 function User() {
+  const [ component, setComponent ] = useState(null)
+
+  useEffect(() => {
+    const subscription = userService.getUser()
+      .subscribe(
+        user => {
+          console.log('user update', user)
+          if (user) {
+            // set profile component
+          } else {
+            // set login/signup buttons component
+          }
+        },
+        error => console.log('user error', error)
+      )
+
+    return () => subscription.unsubscribe()
+  }, [])
+
   return (
-    <main className="route User">
-      Test User
+    <main className="route user">
+      { component ? component : <div>Error: Missing Component</div> }
     </main>
   )
 }
