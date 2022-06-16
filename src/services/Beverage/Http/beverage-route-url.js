06d47bf3484008ddd638df26d55208d02d15c93a
Andrew Wanex <awanex2705@gmail.com>
2022-06-16 16:20:20 -0700

    refactor: move beverage route url to beverage service folder

---
+++ src/services/Beverage/Http/beverage-route-url.js
@@ -0,0 +1,3 @@
+import { BASE_URL } from '../../../shared/constants/base-url'
+
+export const beverageRouteURL = `${BASE_URL}/beverages`
--- src/shared/urls/beverage.js
+++
@@ -1,3 +0,0 @@
-import { BASE_URL } from './base-url'
-
-export const beverageRouteURL = `${BASE_URL}/beverages`
