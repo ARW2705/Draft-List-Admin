5ff4486f0662a034e802204664a282894634c5ab
Andrew Wanex <awanex2705@gmail.com>
2022-06-16 17:04:46 -0700

    feature: setup initial state

--- src/routing/routes/Beverages/Beverages.jsx
+++ src/routing/routes/Beverages/Beverages.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { getAuthoredBeverages } from '../../../services/Beverage/Beverage'
 
 
 function Beverages() {
+  const [ authoredListPage, setAuthoredListPage ] = useState(0)
+  const [ pageCount, setPageCount ] = useState(25)
+  const [ beverageList, setBeverageList ] = useState([])
+
+  useEffect(() => {
+    async function getAuthored() {
+      const beverages = await getAuthoredBeverages(authoredListPage, pageCount)
+      console.log(beverages)
+      setBeverageList(beverages)
+    }
+    getAuthored()
+  }, [ authoredListPage, pageCount ])
+
   return (
     <main className="route Beverages">
       Test Beverages
