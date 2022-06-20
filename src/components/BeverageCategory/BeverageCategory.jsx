4752646711984037a12a5a0d545dfd84866419bc
Andrew Wanex <awanex2705@gmail.com>
2022-06-20 12:48:01 -0700

    feature: add initial beverage category select component

---
+++ src/components/BeverageCategory/BeverageCategory.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+import Button from '../Common/Button/Button'
+
+import './BeverageCategory.css'
+
+
+function BeverageCategory({ handleSelectCategory }) {
+  const baseClass = 'category-button'
+
+  const [buttonClasses, setButtonClasses] = useState({
+    authored: `${baseClass} selected`,
+    previous: baseClass,
+    search: baseClass
+  })
+
+  const handleClick = ({ target }) => {
+    const name = target.name.split('-')[0]
+    setButtonClasses({
+      authored: `${baseClass} ${name === 'authored' ? 'selected': ''}`,
+      previous: `${baseClass} ${name === 'previous' ? 'selected': ''}`,
+      search: `${baseClass} ${name === 'search' ? 'selected': ''}`
+    })
+    handleSelectCategory(name)
+  }
+
+  return (
+    <section
+      className='BeverageCategory'
+      onClick={ handleClick }
+    >
+      <Button
+        text='Authored Beverages'
+        name='authored-category'
+        customClass={ buttonClasses.authored }
+      />
+      <Button
+        text='Previous Beverages'
+        name='previous-category'
+        customClass={ buttonClasses.previous }
+      />
+      <Button
+        text='Search Beverages'
+        name='search-category'
+        customClass={ buttonClasses.search }
+      />
+    </section>
+  )
+}
+
+
+export default React.memo(BeverageCategory)
