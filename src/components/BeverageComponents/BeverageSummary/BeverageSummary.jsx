a1eee1a75b46328f54dd5d7cd2073926e5291eb3
Andrew Wanex <awanex2705@gmail.com>
2022-07-12 18:44:39 -0700

    chore: minor code formatting

--- src/components/BeverageComponents/BeverageSummary/BeverageSummary.jsx
+++ src/components/BeverageComponents/BeverageSummary/BeverageSummary.jsx
@@ -7,21 +7,15 @@ function BeverageSummary({ abv, ibu, srm, description }) {
   return (
     <div className='beverage-summary'>
       <div className='number-values'>
-        {
-          <span>
-            ABV <span className='value'>{ abv ? `${abv}%` : '--' }</span>
-          </span>
-        }
-        {
-          <span>
-            IBU <span className='value'>{ ibu ? ibu : '--' }</span>
-          </span>
-        }
-        {
-          <span>
-            SRM <span className='value'>{ srm ? srm : '--' }</span>
-          </span>
-        }
+        <span>
+          ABV <span className='value'>{ abv ? `${abv}%` : '--' }</span>
+        </span>
+        <span>
+          IBU <span className='value'>{ ibu ? ibu : '--' }</span>
+        </span>
+        <span>
+          SRM <span className='value'>{ srm ? srm : '--' }</span>
+        </span>
       </div>
       { description && <p>{ description }</p> }
     </div>
