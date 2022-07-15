e2d62dee7a8c30fe8e6320280fe4ffc17d57e13c
Andrew Wanex <awanex2705@gmail.com>
2022-07-15 12:43:46 -0700

    feature: add base device list component

---
+++ src/components/DeviceComponents/DeviceList/DeviceList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './DeviceList.css'
+
+
+function DeviceList() {
+  return (
+    <div className='device-list-container'>
+      Devices
+    </div>
+  )
+}
+
+
+export default React.memo(DeviceList)
