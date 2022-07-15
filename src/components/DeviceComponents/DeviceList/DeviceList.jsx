afb0956266f4146f30f24b30ac12d74f269acc15
Andrew Wanex <awanex2705@gmail.com>
2022-07-15 15:07:44 -0700

    feature: connect device service to device list component

--- src/components/DeviceComponents/DeviceList/DeviceList.jsx
+++ src/components/DeviceComponents/DeviceList/DeviceList.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+
+import Device from '../Device/Device'
+
+import { getDevices } from '../../../services/Device/Device'
 
 import './DeviceList.css'
 
 
 function DeviceList() {
+  const [ components, setComponents ] = useState([])
+
+  const buildComponents = useCallback(devices => {
+    if (!devices.length) return <p className='empty-list'>Nothing here...</p>
+  
+    return devices.map(device => {
+      return (
+        <Device
+          className='device-container'
+          key={ device._id }
+          device={ device }
+        />
+      )
+    })
+  }, [])
+
+  useEffect(() => {
+    async function getList() {
+      const { devices, errors } = getDevices()
+      setComponents(buildComponents(devices))
+    }
+    getList()
+  }, [buildComponents])
+
   return (
     <div className='device-list-container'>
-      Devices
+      { components }
     </div>
   )
 }
