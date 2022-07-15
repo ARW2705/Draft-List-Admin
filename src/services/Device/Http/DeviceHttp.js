3f8c086d5b5142548d549890c08d53132b3df05a
Andrew Wanex <awanex2705@gmail.com>
2022-07-15 15:28:56 -0700

    refactor: update device post and patch methods to use multipart/form-data instead of json

--- src/services/Device/Http/DeviceHttp.js
+++ src/services/Device/Http/DeviceHttp.js
@@ -2,16 +2,34 @@ import { get, patch, post } from '../../HttpClient/HttpClient'
 import { deviceRouteURL } from './device-route-url'
 
 
+function buildFormData(beverageData) {
+  const { data, image } = beverageData
+  const formData = new FormData()
+  formData.append('data', JSON.stringify(data))
+  if (image) {
+    formData.append('image', image)
+  }
+  return formData
+}
+
 async function getDeviceById(id) {
   return await get(`${deviceRouteURL}/${id}`)
 }
 
 async function postDevice(deviceData) {
-  return await post(deviceRouteURL, deviceData)
+  return await post(
+    deviceRouteURL,
+    buildFormData(deviceData),
+    { 'Content-Type': 'multipart/form-data' }
+  )
 }
 
-async function patchDevice(device) {
-  return await patch(`${deviceRouteURL}/${device._id}`, device)
+async function patchDevice(deviceId, deviceData) {
+  return await patch(
+    `${deviceRouteURL}/${deviceId}`,
+    buildFormData(deviceData),
+    { 'Content-Type': 'multipart/form-data' }
+  )
 }
 
 async function confirmDevice(confirmation) {
