850118c69e4eba61568d7fb3b59b32bf43f77442
Andrew Wanex <awanex2705@gmail.com>
2022-07-18 14:50:35 -0700

    feature: add basic device component

--- src/components/DeviceComponents/Device/Device.jsx
+++ src/components/DeviceComponents/Device/Device.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import Locale from '../../Locale/Locale'
+
+import { IMAGE_BASE_URL } from '../../../shared/constants/image-base-url'
 
 import './Device.css'
 
 
-function Device({ device }) {
+function Device({ device, onClick: handleOnClick }) {
   const { name, title, imageURL, locale, draftList } = device
-  const { city, region, country } = locale
+  const [ activeDraftCount, setActiveDraftCount ] = useState(null)
+  const [ fullImageURL, setFullImageURL ] = useState('')
+  
+  useEffect(() => {
+    setFullImageURL(`${IMAGE_BASE_URL}/${imageURL}`)
+  }, [imageURL])
+  
+  useEffect(() => {
+    setActiveDraftCount(draftList.reduce((acc, curr) => acc + curr.isActive ? 1 : 0, 0))
+  }, [draftList])
 
   return (
-    <article className='device'>
-      
+    <article
+      className='device'
+      onClick={ () => handleOnClick(device) }
+    >
+      <img
+        className='device-image'
+        src={ fullImageURL }
+        alt='device logo'
+      />
+      <div className='device-identifier'>
+        <span>{ title ?? name }</span>
+        <Locale { ...locale } />
+      </div>
+      <div className='active-draft-count'>
+        Active drafts: <span>{ activeDraftCount }</span>
+      </div>
     </article>
   )
 }
