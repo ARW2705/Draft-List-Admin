d2c398de770bc14ea19c1ffcf9c097d6574819af
Andrew Wanex <awanex2705@gmail.com>
2022-07-26 18:24:01 -0700

    refactor: update image service to contain image worker

--- src/services/Image/Image.js
+++ src/services/Image/Image.js
@@ -1,5 +1,17 @@
 import { get } from '../HttpClient/HttpClient'
+import base64ToBlobWorker from '../../shared/workers/base64-to-blob.worker'
 
+
+async function blobifyBase64Image(image64) {
+  const contentType = RegExp(/(?:data:)(.*)(?:;)/g).exec(image64)[1]
+  return new Promise((resolve, _) => {
+    base64ToBlobWorker.onmessage = ({ data }) => resolve(data)
+    base64ToBlobWorker.postMessage({
+      contentType,
+      image64
+    })
+  })
+}
 
 async function fetchImageAsBase64(url) {
   const response = await get(url, {}, {}, 'blob')
@@ -16,5 +28,6 @@ async function getImage(url) {
 
 
 export {
-  getImage
+  getImage,
+  blobifyBase64Image
 }
