84f4e7fe0678d04ecb3836aca257e22383770f0a
Andrew Wanex <awanex2705@gmail.com>
2022-07-29 17:37:29 -0700

    refactor: add loading spinner

--- src/components/BeverageComponents/BeverageList/BeverageList.jsx
+++ src/components/BeverageComponents/BeverageList/BeverageList.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { getAuthoredBeverages, getPreviousBeverages, getBeveragesByQuery } from '../../../services/Beverage/Beverage'
 
 import Beverage from '../../BeverageComponents/Beverage/Beverage'
+import SpinnerLoader from '../../Common/Loaders/Spinner/Spinner'
 
 import { LIST_TYPE_ENUM } from '../../../shared/constants/list-type-enum'
 
@@ -13,6 +14,7 @@ import './BeverageList.css'
 function BeverageList({ listConfig }) {
   const { listType, pageNum, pageCount, searchType, searchTerm } = listConfig
   const [ components, setComponents ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(true)
 
   const location = useLocation()
   const navigate = useNavigate()
@@ -20,7 +22,12 @@ function BeverageList({ listConfig }) {
     navigate(`${location.pathname}/form`, { state: { beverage }})
   }, [navigate, location.pathname])
 
-  const buildComponents = useCallback(beverages => {
+  const buildComponents = useCallback((beverages, loadStartTime) => {
+    const minLoaderTime = 500 // in ms
+    setTimeout(() => {
+      setIsLoading(false)
+    }, Date.now() - loadStartTime + minLoaderTime)
+
     if (!beverages.length) return <p className='empty-list'>Nothing here...</p>
 
     return beverages.map(beverage => {
@@ -36,19 +43,21 @@ function BeverageList({ listConfig }) {
   }, [handleOnClick])
 
   useEffect(() => {
-    async function getQuery() {
+    async function getQuery(loadStartTime) {
       const { beverages, errors } = await getBeveragesByQuery(searchType, searchTerm, pageNum, pageCount)
-      setComponents(buildComponents(beverages))
+      setComponents(buildComponents(beverages, loadStartTime))
     }
 
     if (listType === LIST_TYPE_ENUM.Search && searchType && searchTerm) {
-      getQuery()
+      getQuery(Date.now())
     }
   }, [listType, searchType, searchTerm, pageNum, pageCount, buildComponents])
 
   useEffect(() => {
-    async function getList() {
-      if (listType === LIST_TYPE_ENUM.Search) return
+    async function getList(loadStartTime) {
+      if (listType === LIST_TYPE_ENUM.Search) {
+        return
+      }
       
       let getter = { beverages: [], errors: [] }
       if (listType === LIST_TYPE_ENUM.Authored) {
@@ -61,14 +70,14 @@ function BeverageList({ listConfig }) {
 
       const { beverages, errors } = getter
       console.log('beverage list error', errors)
-      setComponents(buildComponents(beverages))
+      setComponents(buildComponents(beverages, loadStartTime))
     }
-    getList()
+    getList(Date.now())
   }, [listType, pageNum, pageCount, buildComponents])
 
   return (
     <div className='beverage-list'>
-      { components }
+      { isLoading ? <SpinnerLoader /> : components }
     </div>
   )
 }
