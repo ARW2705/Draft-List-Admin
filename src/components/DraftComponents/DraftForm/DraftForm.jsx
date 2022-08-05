bb8f62bd0ac3693dd0c4f2c718a21fba07575597
Andrew Wanex <awanex2705@gmail.com>
2022-08-05 16:19:46 -0700

    refactor: change draft form from form group component to a custom selection

--- src/components/DraftComponents/DraftForm/DraftForm.jsx
+++ src/components/DraftComponents/DraftForm/DraftForm.jsx
@@ -3,32 +3,20 @@ import { useNavigate, useLocation } from 'react-router-dom'
 
 import FormGroup from '../../Common/Form/FormGroup/FormGroup'
 import Spinner from '../../Common/Loaders/Spinner/Spinner'
+import SearchBar from '../../Common/SearchBar/SearchBar'
+import BeverageSimpleView from '../../BeverageComponents/BeverageSimpleView/BeverageSimpleView'
 
 import { addNewDraft, updateDraft } from '../../../services/Draft/Draft'
+import { getPreviousBeverages, getBeveragesByQuery } from '../../../services/Beverage/Beverage'
 import createForm from '../../../shared/form/create-form'
+import Button from '../../Common/Button/Button'
 
 
 function DraftForm() {
   const [ isLoading, setIsLoading ] = useState(false)
-  const formData = useRef()
-  const location = useLocation()
-  const draft = location.state?.draft
-
-  let fields = {
-    beverage: {
-      value: draft?.beverage || '',
-      element: 'select',
-      options: {
-        formOptions: [
-          { label: 'test1', value: 1 },
-          { label: 'test2', value: 2 },
-          { label: 'test3', value: 3 }
-        ]
-      }
-    }
-  }
-
-  const form = createForm({ fields })
+  const [ previousBeverages, setPreviousBeverages ] = useState(null)
+  const [ searchResult, setSearchResult ] = useState(null)
+  const formData = useRef({ beverage: null, container: null })
 
   const navigate = useNavigate()
   const navigateBack = useCallback(() => {
@@ -40,24 +28,53 @@ function DraftForm() {
 
   }, [navigateBack])
 
+  const buildPreviousList = useCallback(beverageList => {
+    if (!beverageList.length) return <div>No previously used beverages...</div>
+
+    return (
+      <div className='previous-beverage-list-container'>
+        {
+          beverageList.map(beverage => (
+            <BeverageSimpleView
+              key={ beverage._id }
+              onClick={ handleBeverageClick }
+              { ...beverage }
+            />
+          ))
+        }
+      </div>
+    )
+  }, [])
+
   useEffect(() => {
-    if (isLoading && formData.current) {
-      submitForm()
+    async function getRecentBeverages() {
+      const { beverages, errors } = await getPreviousBeverages(0, 5)
+      console.log('got previous', beverages)
+      setPreviousBeverages(buildPreviousList(beverages))
     }
-  }, [isLoading, submitForm])
+    getRecentBeverages()
+  }, [buildPreviousList])
   
-  const handleSubmit = async data => {
-    if (!data) {
-      // navigateBack()
-    } else {
-      formData.current = data
-      // setIsLoading(true)
-    }
-    console.log(formData.current)
+  const handleSearchOnSubmit = async searchTerm => {
+    console.log('search', searchTerm)
+    const { beverages, errors } = await getBeveragesByQuery('name', searchTerm, 0, 1)
+    setSearchResult(
+      beverages.length
+      ? <BeverageSimpleView
+          onClick={ handleBeverageClick }
+          { ...beverages[0] }
+        />
+      : <div>{ `'${searchTerm}' not found` }</div>
+    )
+  }
+
+  const handleBeverageClick = (_, beverageId) => {
+    console.log(beverageId)
+    formData.current = { ...formData.current, beverage: beverageId }
   }
 
   return (
-    <>
+    <div className='draft-form-container'>
       {
         isLoading
         && (
@@ -67,13 +84,20 @@ function DraftForm() {
           />
         )
       }
-      <FormGroup
-        form={ form }
-        submitHandler={ handleSubmit }
-        customClass='draft'
-        title='Draft Form'
-      />
-    </>
+      <div className='beverage-selection-container'>
+        <SearchBar
+          handleOnSubmit={ handleSearchOnSubmit }
+          label='Beverage Name'
+        />
+        { searchResult ?? <></> }
+        <div className='most-recent-list-container'>
+          { previousBeverages ?? <Spinner /> }
+        </div>
+      </div>
+      <div className='container-selection-contaienr'>
+
+      </div>
+    </div>
   )
 }
 
