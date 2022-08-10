de1e62b6c32b6f1e95deb91cc9125a1425c2af41
Andrew Wanex <awanex2705@gmail.com>
2022-08-09 17:34:42 -0700

    feature: break out container selection to its own component

---
+++ src/components/DraftComponents/DraftForm/ContainerSelect/ContainerSelect.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+
+import DropDown from '../../../Common/DropDown/DropDown'
+
+import { getAllContainers } from '../../../../services/Container/Container'
+
+import './ContainerSelect.css'
+
+
+function ContainerSelect({ onSelect: handleOnSelect }) {
+  const [ containerOptions, setContainerOptions ] = useState([])
+  const [ containers, setContainers ] = useState([])
+
+  const handleSelect = name => {
+    for (const key in containers) {
+      if (containers[key].name.toLowerCase() === name.toLowerCase()) {
+        handleOnSelect(containers[key])
+      }
+    }
+  }
+
+  useEffect(() => {
+    async function getContainers() {
+      const containers = await getAllContainers()
+      let containerNames = []
+      for (const key in containers) {
+        containerNames = [...containerNames, containers[key].name]
+      }
+      setContainers(containers)
+      setContainerOptions(containerNames)
+    }
+    getContainers()
+  }, [])
+
+  return (
+    <div className='container-selection-container'>
+      <DropDown
+        title='Select a Container'
+        items={ containerOptions }
+        customClass='container-dropdown'
+        onSelect={ handleSelect }
+      />
+    </div>
+  )
+}
+
+
+export default React.memo(ContainerSelect)
