59966d58769366ff27cd124432d72d91b539f23a
Andrew Wanex <awanex2705@gmail.com>
2022-08-19 19:43:57 -0700

    feature: add device preview to selection preview

--- src/components/DraftComponents/DraftForm/SelectionPreview/SelectionPreview.jsx
+++ src/components/DraftComponents/DraftForm/SelectionPreview/SelectionPreview.jsx
@@ -8,6 +8,7 @@ function SelectionPreview({ beverage, container, device }) {
     <div className='selections'>
       <div>Beverage: { beverage && <span>{ beverage.title || beverage.name }</span> }</div>
       <div>Container: { container && <span>{ container.name }</span> }</div>
+      <div>Device: { device && <span>{ device.title || device.name }</span> }</div>
     </div>
   )
 }
