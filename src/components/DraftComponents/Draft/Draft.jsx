ed32a8b1a534e52dee0bcf46a95c1c66e2ef425d
Andrew Wanex <awanex2705@gmail.com>
2022-09-15 13:53:10 -0700

    feature: add loading spinner

--- src/components/DraftComponents/Draft/Draft.jsx
+++ src/components/DraftComponents/Draft/Draft.jsx
@@ -2,26 +2,39 @@ import React, { useEffect, useState } from 'react'
 
 import { getBeverageById } from '../../../services/Beverage/Beverage'
 
+import SpinnerLoader from '../../Common/Loaders/Spinner/Spinner'
+
 import './Draft.css'
 
 
 function Draft({ draft, onClick: handleOnClick }) {
   const { container } = draft
   const { quantity, containerInfo, contentColor } = container
-  const { type, name, capacity } = containerInfo
+  const { type, name: containerName, capacity } = containerInfo
   const [ beverage, setBeverage ] = useState(null)
+  const [ isLoading, setIsLoading ] = useState(true)
 
   useEffect(() => {
     async function getBeverage() {
       setBeverage(await getBeverageById(draft.beverage))
+      setIsLoading(false)
     }
     getBeverage()
-  }, [setBeverage, draft.beverage])
+  }, [draft.beverage])
 
   return (
     <div className='draft-container'>
-      <span>{ beverage.title || beverage.name }</span>
-      <span>{ name }</span>
+      {
+        isLoading
+        ? <SpinnerLoader />
+        : (
+          <>
+            <span>•{ beverage.title || beverage.name }</span>
+            <span>•{ containerName }</span>
+            <span>•Remaining: { Math.floor(quantity * 100 / capacity) } %</span>
+          </>
+        )
+      }
     </div>
   )
 }
