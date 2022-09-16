267581d7a3743e680884a7949fee1adaecb10c54
Andrew Wanex <awanex2705@gmail.com>
2022-09-16 16:07:36 -0700

    feature: finalize draft component content

--- src/components/DraftComponents/Draft/Draft.jsx
+++ src/components/DraftComponents/Draft/Draft.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 import { getBeverageById } from '../../../services/Beverage/Beverage'
 
+import Button from '../../Common/Button/Button'
+import Image from '../../Common/Image/Image'
 import SpinnerLoader from '../../Common/Loaders/Spinner/Spinner'
 
 import './Draft.css'
 
 
-function Draft({ draft, onClick: handleOnClick }) {
+function Draft({ draft }) {
   const { container } = draft
-  const { quantity, containerInfo, contentColor } = container
-  const { type, name: containerName, capacity } = containerInfo
+  const { quantity, containerInfo } = container
+  const { name: containerName, capacity } = containerInfo
   const [ beverage, setBeverage ] = useState(null)
   const [ isLoading, setIsLoading ] = useState(true)
 
@@ -22,6 +25,25 @@ function Draft({ draft, onClick: handleOnClick }) {
     getBeverage()
   }, [draft.beverage])
 
+  const location = useLocation()
+  const navigate = useNavigate()
+  const handleOnClick = ({ name }) => {
+    switch (name) {
+      case 'change-quantity':
+        console.log('change')
+        break
+      case 'edit-draft':
+        console.log('edit')
+        navigate(`${location.pathname}/form`, { state: { draft }})
+        break
+      case 'finish-draft':
+        console.log('finish')
+        break
+      default:
+        throw new Error(`Invalid click event: ${name}`)
+    }
+  }
+
   return (
     <div className='draft-container'>
       {
@@ -29,9 +51,34 @@ function Draft({ draft, onClick: handleOnClick }) {
         ? <SpinnerLoader />
         : (
           <>
-            <span>•{ beverage.title || beverage.name }</span>
-            <span>•{ containerName }</span>
-            <span>•Remaining: { Math.floor(quantity * 100 / capacity) } %</span>
+            <Image
+              imageURL={ beverage.imageURL }
+              alt='beverage label'
+              customClass='draft-beverage-label'
+            />
+            <div className='draft-content-a'>{ beverage.title || beverage.name }</div>
+            <div className='draft-content-b'>•</div>
+            <div className='draft-content-c'>{ containerName }</div>
+            <div className='draft-content-d'>•</div>
+            <div className='draft-content-e'>{ Math.floor(quantity * 100 / capacity) }%</div>
+            <Button
+              text='Change Quantity'
+              name='change-quantity'
+              customClass='draft-button-a'
+              onClick={ handleOnClick }
+            />
+            <Button
+              text='Edit Draft'
+              name='edit-draft'
+              customClass='draft-button-b'
+              onClick={ handleOnClick }
+            />
+            <Button
+              text='Finish Draft'
+              name='finish-draft'
+              customClass='draft-button-c'
+              onClick={ handleOnClick }
+            />
           </>
         )
       }
