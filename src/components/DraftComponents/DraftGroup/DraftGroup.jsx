cc808ac7e7a464f99a4cebcd7ba1e1e79085252e
Andrew Wanex <awanex2705@gmail.com>
2022-09-16 16:09:02 -0700

    refactor: move new draft button from router to draft group component

--- src/components/DraftComponents/DraftGroup/DraftGroup.jsx
+++ src/components/DraftComponents/DraftGroup/DraftGroup.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+import Button from '../../Common/Button/Button'
 
 import './DraftGroup.css'
 
 
-function DraftGroup({ deviceName, draftComponents }) {
+function DraftGroup({ deviceName, deviceId, draftComponents }) {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const handleOnClick = () => {
+    navigate(`${location.pathname}/form`, { state: { deviceId }})
+  }
+  
   return (
     <div className='draft-group-container'>
       <h2 className='draft-device-name'>{ deviceName }</h2>
+      <Button
+        text='Add New Draft'
+        name='add-draft'
+        onClick={ handleOnClick }
+        customClass='new-draft-button'
+        isFlat={ true }
+      />
       <div className='draft-group'>
         { draftComponents }
       </div>
--- src/routing/routes/Drafts/Drafts.jsx
+++ src/routing/routes/Drafts/Drafts.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 import DraftList from '../../../components/DraftComponents/DraftList/DraftList'
 import DraftForm from '../../../components/DraftComponents/DraftForm/DraftForm'
-import Button    from '../../../components/Common/Button/Button'
 
 import './Drafts.css'
 
 
 function Drafts() {
-  const location = useLocation()
-  const navigate = useNavigate()
-  const handleOnClick = () => {
-    navigate(`${location.pathname}/form`)
-  }
-
   return (
     <main className='route drafts-router'>
       <Routes>
@@ -22,11 +15,6 @@ function Drafts() {
           path='/'
           element={
             <div className='drafts-container'>
-              <Button
-                text='Add New Draft'
-                name='new-draft'
-                onClick={ handleOnClick }
-              />
               <DraftList />
             </div>
           }
