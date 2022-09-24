1a71af8437c5e42d52b65d89bec4af690301f116
Andrew Wanex <awanex2705@gmail.com>
2022-09-23 17:06:20 -0700

    refactor: update beverage list to single page type due to each user controlling their own beverage

--- src/components/BeverageComponents/BeverageList/BeverageList.jsx
+++ src/components/BeverageComponents/BeverageList/BeverageList.jsx
@@ -1,18 +1,16 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
-import { getAuthoredBeverages, getPreviousBeverages, getBeveragesByQuery } from '../../../services/Beverage/Beverage'
+import { getBeverageList, getBeveragesByQuery } from '../../../services/Beverage/Beverage'
 
 import Beverage from '../../BeverageComponents/Beverage/Beverage'
 import SpinnerLoader from '../../Common/Loaders/Spinner/Spinner'
-
-import { LIST_TYPE_ENUM } from '../../../shared/constants/list-type-enum'
 
 import './BeverageList.css'
 
 
 function BeverageList({ listConfig }) {
-  const { listType, pageNum, pageCount, searchType, searchTerm } = listConfig
+  const { pageNum, pageCount, searchType, searchTerm } = listConfig
   const [ components, setComponents ] = useState([])
   const [ isLoading, setIsLoading ] = useState(true)
 
@@ -23,7 +21,7 @@ function BeverageList({ listConfig }) {
   }, [navigate, location.pathname])
 
   const buildComponents = useCallback((beverages, loadStartTime) => {
-    const minLoaderTime = 500 // in ms
+    const minLoaderTime = 250 // in ms
     setTimeout(() => {
       setIsLoading(false)
     }, Date.now() - loadStartTime + minLoaderTime)
@@ -43,37 +41,20 @@ function BeverageList({ listConfig }) {
   }, [handleOnClick])
 
   useEffect(() => {
-    async function getQuery(loadStartTime) {
-      const { beverages, errors } = await getBeveragesByQuery(searchType, searchTerm, pageNum, pageCount)
-      setComponents(buildComponents(beverages, loadStartTime))
-    }
-
-    if (listType === LIST_TYPE_ENUM.Search && searchType && searchTerm) {
-      getQuery(Date.now())
-    }
-  }, [listType, searchType, searchTerm, pageNum, pageCount, buildComponents])
-
-  useEffect(() => {
-    async function getList(loadStartTime) {
-      if (listType === LIST_TYPE_ENUM.Search) {
-        return
-      }
-      
-      let getter = { beverages: [], errors: [] }
-      if (listType === LIST_TYPE_ENUM.Authored) {
-        getter = await getAuthoredBeverages(pageNum, pageCount)
-      } else if (listType === LIST_TYPE_ENUM.Previous) {
-        getter = await getPreviousBeverages(pageNum, pageCount)
+    async function getList(loadStartTime, isQuery) {
+      if (isQuery) {
+        const { beverages, errors } = await getBeveragesByQuery(searchType, searchTerm, pageNum, pageCount)
+        setComponents(buildComponents(beverages, loadStartTime))
       } else {
-        throw new Error (`Unknown Beverage list type: ${listType}`)
+        const { beverages, errors } = await getBeverageList(pageNum, pageCount)
+        setComponents(buildComponents(beverages, loadStartTime))
       }
-
-      const { beverages, errors } = getter
-      console.log('beverage list error', errors)
-      setComponents(buildComponents(beverages, loadStartTime))
     }
-    getList(Date.now())
-  }, [listType, pageNum, pageCount, buildComponents])
+
+    if ((searchType && searchTerm) || (!searchType && !searchTerm)) {
+      getList(Date.now(), searchTerm && searchType)
+    }
+  }, [searchType, searchTerm, pageNum, pageCount, buildComponents])
 
   return (
     <div className='beverage-list'>
