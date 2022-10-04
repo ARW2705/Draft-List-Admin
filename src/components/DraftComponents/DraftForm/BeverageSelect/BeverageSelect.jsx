14eac8eafb6332eb0dc61dd13b7bdbe6446ea569
Andrew Wanex <awanex2705@gmail.com>
2022-10-03 19:05:30 -0700

    refactor: change beverage select layout to a card view

--- src/components/DraftComponents/DraftForm/BeverageSelect/BeverageSelect.jsx
+++ src/components/DraftComponents/DraftForm/BeverageSelect/BeverageSelect.jsx
@@ -4,7 +4,8 @@ import SearchBar  from '../../../Common/SearchBar/SearchBar'
 import SimpleView from '../../../Common/SimpleView/SimpleView'
 import Spinner    from '../../../Common/Loaders/Spinner/Spinner'
 
-import { getBeverageById, getPreviousBeverages, getBeveragesByQuery } from '../../../../services/Beverage/Beverage'
+import { getBeverageById, getBeverageList, getBeveragesByQuery } from '../../../../services/Beverage/Beverage'
+import toTitleCase from '../../../../shared/utilities/title-case'
 
 import './BeverageSelect.css'
 
@@ -14,7 +15,7 @@ function BeverageSelect({ onSelect: handleOnSelect }) {
   const [ searchResult, setSearchResult ] = useState(null)
   const [ previousBeverages, setPreviousBeverages ] = useState(null)
 
-  const handleClick = useCallback(async (_, { _id }) => {
+  const handleClick = useCallback(async _id => {
     handleOnSelect(await getBeverageById(_id))
   }, [handleOnSelect])
 
@@ -22,40 +23,53 @@ function BeverageSelect({ onSelect: handleOnSelect }) {
     if (!beverageList.length) return <div>No previously used beverages...</div>
 
     return (
-      <div className='previous-beverage-list-container'>
+      <ul className='previous-beverage-list'>
         {
-          beverageList.map(beverage => (
-            <SimpleView
-              key={ beverage._id }
-              onClick={ handleClick }
-              keysToDisplay={ [beverage.title ? 'title' : 'name', 'source', 'style'] }
-              data={ beverage }
-              customClass='beverage-view'
-            />
+          beverageList.map((beverage, index) => (
+            <li
+              key={ index }
+              className='beverage-list-item'
+            >
+              <button
+                className='list-button'
+                onClick={ () => handleClick(beverage._id) }
+              >
+                <span>{ toTitleCase(beverage.title || beverage.name) }</span>
+                <span>{ toTitleCase(beverage.source) }</span>
+                <span>{ toTitleCase(beverage.style) }</span>
+              </button>
+            </li>
           ))
         }
-      </div>
+      </ul>
     )
   }, [handleClick])
 
   const handleSearchOnSubmit = async searchTerm => {
-    const { beverages, errors } = await getBeveragesByQuery('name', searchTerm, 0, 1)
-    const beverage = beverages[0]
-    setSearchResult(
-      beverages.length
-      ? <SimpleView
-          onClick={ handleClick }
-          keysToDisplay={ [beverage.title ? 'title' : 'name', 'source', 'style'] }
-          data={ beverage }
-          customClass='beverage-view'
-        />
-      : <div>{ `'${searchTerm}' not found` }</div>
-    )
+    let result = <></>
+    if (searchTerm !== null) {
+      const { beverages, errors } = await getBeveragesByQuery('name', searchTerm, 0, 1)
+      const beverage = beverages[0]
+      if (beverage) {
+        result = (
+          <SimpleView
+            keysToDisplay={ [beverage.title ? 'title' : 'name', 'source', 'style'] }
+            data={ beverage }
+            customClass='beverage-view'
+          />
+        )
+        handleOnSelect(beverage)
+      } else {
+        result = <div>{ `'${searchTerm}' not found` }</div>
+      }
+    }
+
+    setSearchResult(result)
   }
 
   useEffect(() => {
     async function getRecentBeverages() {
-      const { beverages, errors } = await getPreviousBeverages(0, 5)
+      const { beverages, errors } = await getBeverageList(0, 5)
       console.log('got previous', beverages)
       setPreviousBeverages(buildPreviousList(beverages))
     }
@@ -67,13 +81,15 @@ function BeverageSelect({ onSelect: handleOnSelect }) {
   }, [buildPreviousList])
 
   return (
-    <div className='beverage-selection-container'>
+    <div className='beverage-selection'>
+      <h2>Select a Beverage</h2>
       <SearchBar
         handleOnSubmit={ handleSearchOnSubmit }
         label='Beverage Name'
       />
       { searchResult ?? <></> }
-      <div className='most-recent-list-container'>
+      <div className='most-recent-list'>
+        <h3>Most Recent Beverages</h3>
         { previousBeverages ?? <Spinner /> }
       </div>
     </div>
