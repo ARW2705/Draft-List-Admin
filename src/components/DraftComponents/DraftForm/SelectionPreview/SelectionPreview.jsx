045cd20877863da04c8623beaf7de6e385701f83
Andrew Wanex <awanex2705@gmail.com>
2022-10-03 19:02:11 -0700

    refactor: change draft selection preview view layout and update styling

--- src/components/DraftComponents/DraftForm/SelectionPreview/SelectionPreview.jsx
+++ src/components/DraftComponents/DraftForm/SelectionPreview/SelectionPreview.jsx
@@ -6,9 +6,19 @@ import './SelectionPreview.css'
 function SelectionPreview({ beverage, container, device }) {
   return (
     <div className='selections'>
-      <div>Beverage: { beverage && <span>{ beverage.title || beverage.name }</span> }</div>
-      <div>Container: { container && <span>{ container.name }</span> }</div>
-      <div>Device: { device && <span>{ device.title || device.name }</span> }</div>
+      <h3>Draft Preview</h3>
+      <div className={ `preview-selection ${ device ? '' : 'missing'}` }>
+        <span>Device</span>
+        <span>{ device ? (device.title || device.name) : 'None' }</span>
+      </div>
+      <div className={ `preview-selection ${ beverage ? '' : 'missing'}` }>
+        <span>Beverage</span>
+        <span>{ beverage ? (beverage.title || beverage.name) : 'None' }</span>
+      </div>
+      <div className={ `preview-selection ${ container ? '' : 'missing'}` }>
+        <span>Container</span>
+        <span>{ container ? container.name : 'None' }</span>
+      </div>
     </div>
   )
 }
