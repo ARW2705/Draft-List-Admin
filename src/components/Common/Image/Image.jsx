52720e08b859504b5427e228b68a1c928cb8b796
Andrew Wanex <awanex2705@gmail.com>
2022-10-04 16:39:44 -0700

    feature: add default image and image error handling to image component

--- src/components/Common/Image/Image.jsx
+++ src/components/Common/Image/Image.jsx
@@ -2,28 +2,38 @@ import React, { useEffect, useState } from 'react'
 
 import { getImage } from '../../../services/Image/Image'
 
+import defaultImage from '../../../assets/images/hops-and-grains_1280x640.png'
+
 import './Image.css'
 
 
 function Image({ imageURL, alt, customClass }) {
-  const [ fullImageURL, setFullImageURL ] = useState('')
+  const defaultImageAlt = 'hops and grains'
+  const [ imageAlt, setImageAlt ] = useState(defaultImageAlt)
+  const [ fullImageURL, setFullImageURL ] = useState(defaultImage)
 
   useEffect(() => {
     if (imageURL) {
       async function getAsyncImage() {
+        setImageAlt(alt || imageURL)
         setFullImageURL(await getImage(imageURL))
       }
       getAsyncImage()
-    } else {
-      setFullImageURL('')
     }
-  }, [imageURL])
+  }, [imageURL, alt])
+
+  const handleImageError = error => {
+    console.log('image not found', error.target.src)
+    setImageAlt(defaultImageAlt)
+    setFullImageURL(defaultImage)
+  }
 
   return (
     <img
       className={ `image ${customClass}` }
       src={ fullImageURL }
-      alt={ alt || imageURL }
+      alt={ imageAlt }
+      onError={ handleImageError }
     />
   )
 }
