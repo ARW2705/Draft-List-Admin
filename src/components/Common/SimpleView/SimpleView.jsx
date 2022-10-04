026af38845126020773fdb555273d155a244b214
Andrew Wanex <awanex2705@gmail.com>
2022-10-03 19:05:55 -0700

    refactor: remove click handling from simple view component

--- src/components/Common/SimpleView/SimpleView.jsx
+++ src/components/Common/SimpleView/SimpleView.jsx
@@ -5,7 +5,7 @@ import toTitleCase from '../../../shared/utilities/title-case'
 import './SimpleView.css'
 
 
-function SimpleView({ keysToDisplay, data, customClass, onClick: handleOnClick }) {
+function SimpleView({ keysToDisplay, data, customClass }) {
   const [ datapoints, setDatapoints ] = useState([])
   
   useEffect(() => {
@@ -20,10 +20,7 @@ function SimpleView({ keysToDisplay, data, customClass, onClick: handleOnClick }
   }, [keysToDisplay, data])
 
   return (
-    <div
-      className={ `simple-view-container ${customClass || ''}` }
-      onClick={ e => handleOnClick(e, data) }
-    >
+    <div className={ `simple-view-container ${customClass || ''}` }>
       { datapoints }
     </div>
   )
