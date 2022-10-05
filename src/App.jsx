db25bab31075ee7d1cdfb23b914211e8e72bbdcf
Andrew Wanex <awanex2705@gmail.com>
2022-10-05 15:38:42 -0700

    feature: setup default routing to base route to redirect to either drafts or user page depending on user login status

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate, Outlet } from 'react-router-dom'
 
-import token from './services/Token/Token'
-import user from './services/User/User'
+import Token from './services/Token/Token'
+import User from './services/User/User'
 
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
@@ -11,8 +11,28 @@ import './App.css'
 
 
 function App() {
-  token.init()
-  user.init()
+  Token.init()
+  User.init()
+
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+
+  const location = useLocation()
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate(isLoggedIn ? '/draft' : '/user')
+    }
+  }, [isLoggedIn, location, navigate])
+
+  useEffect(() => {
+    const subscription = User.getUser()
+      .subscribe({
+        next: user => setIsLoggedIn(user._id !== null),
+        error: error => console.error('user error', error)
+      })
+
+    return () => subscription.unsubscribe()
+  }, [])
 
   return (
     <div className="App">
