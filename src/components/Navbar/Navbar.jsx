3cd11b27db46f6aee3d9cdfeeb3fdd016d2e7728
Andrew Wanex <awanex2705@gmail.com>
2022-10-05 17:29:07 -0700

    feature: make home link dynamic depending on user login status

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useMediaQuery } from 'react-responsive'
 
+import UserService from '../../services/User/User'
+
 import Nav from '../Common/Nav/Nav'
 import BurgerMenu from '../Common/BurgerMenu/BurgerMenu'
 
@@ -10,20 +12,32 @@ import './Navbar.css'
 
 function Navbar() {
   const [ overrideOpen, setOverrideOpen ] = useState(false)
+  const [ homeRoute, setHomeRoute ] = useState('/')
   const links = ['Draft', 'Device', 'Beverage', 'User'].map(pageName => {
     return <Nav name={ pageName } key={ pageName } />
   })
   const isSmallScreen = useMediaQuery({ query: '(max-width: 600px)' })
   const location = useLocation()
+  
   useEffect(() => {
     setOverrideOpen({ isOpen: false })
   }, [location.pathname])
+
+  useEffect(() => {
+    const subscription = UserService.getUser()
+    .subscribe({
+      next: user => setHomeRoute(user._id !== null ? '/draft' : '/user'),
+      error: error => console.error('user error', error)
+    })
+
+  return () => subscription.unsubscribe()
+  }, [])
 
   return (
     <nav className='navbar'>
       <Nav
         name='Draft List'
-        route='/'
+        route={ homeRoute }
         customClass='home-link'
       />
       {
