d6e1ff4c6dbf0f0870f377da3d537a94aabb58f5
Andrew Wanex <awanex2705@gmail.com>
2022-10-05 17:07:28 -0700

    refactor: change layout

--- src/components/LoginSignupButtons/LoginSignupButtons.jsx
+++ src/components/LoginSignupButtons/LoginSignupButtons.jsx
@@ -2,38 +2,37 @@ import React from 'react'
 
 import Button from '../Common/Button/Button'
 
+import './LoginSignupButtons.css'
+
 
 function LoginSignupButtons({ handleOnClick, isLoggedIn }) {
   return (
-    <div
-      className='login-signup-buttons-container'
-      
-    >
+    <div className={`login-signup-buttons-container ${ isLoggedIn ? 'logout' : '' }`}>
       {
-        !isLoggedIn
-        && (
-          <>
-            <Button
-              text='Login'
-              customClass='form-button'
-              onClick={ handleOnClick }
-            />
-            <Button
-              text='Signup'
-              customClass='form-button'
-              onClick={ handleOnClick }
-            />
-          </>
-        )
-      }
-      { 
         isLoggedIn
-        && (
+        ? (
           <Button
             text='Logout'
             customClass='form-button'
             onClick={ handleOnClick }
           />
+        )
+        : (
+          <>
+            <h2>Please log in or signup to continue</h2>
+            <div className='login-signup-buttons'>
+              <Button
+                text='Login'
+                customClass='form-button'
+                onClick={ handleOnClick }
+              />
+              <Button
+                text='Signup'
+                customClass='form-button'
+                onClick={ handleOnClick }
+              />
+            </div>
+          </>
         )
       }
     </div>
