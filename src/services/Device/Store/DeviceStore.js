0d761d3a1071edac6e00bda463799761bc9bfd8a
Andrew Wanex <awanex2705@gmail.com>
2022-10-12 12:59:40 -0700

    refactor: change device store to use indexeddb

--- src/services/Device/Store/DeviceStore.js
+++ src/services/Device/Store/DeviceStore.js
@@ -1,48 +1,79 @@
+import {
+  get     as IDB_get,
+  getMany as IDB_getMany,
+  set     as IDB_set,
+  update  as IDB_update,
+  del     as IDB_del,
+  delMany as IDB_delMany
+} from 'idb-keyval'
+
+
 class DeviceStore {
   constructor() {
     if (DeviceStore._instance) return DeviceStore._instance
     DeviceStore._instance = this
-    this.storageKey = 'devices'
-    this.devices = {} // core device defs
+    this.keyStore = 'device-ids'
+    this.init()
   }
 
-  getDevice(id) {
-    return this.devices[id]
+  async init() {
+    const keys = await IDB_get(this.keyStore)
+    if (!keys) await IDB_set(this.keyStore, [])
   }
 
-  getDevices(ids) {
-    return ids.map(id => this.getDevice(id)).filter(device => !!device)
+  async setDevice(device) {
+    await IDB_update(this.keyStore, ids => {
+      if (!ids.includes(device._id)) return [...ids, device._id]
+      return ids
+    })
+    await IDB_set(device._id, device)
   }
 
-  setDevice(device) {
-    if (device.hasOwnProperty('_id')) {
-      device = this.prepareDevice(device)
+  async setDevices(devices) {
+    return await Promise.allSettled(devices.map(async device => {
+      const entry = await IDB_get(device._id)
+      if (entry) {
+        return IDB_update(device._id, () => device)
+      } else {
+        return IDB_set(device._id, device)
+      }
+    }))
+  }
+
+  async deleteDevice(deviceId) {
+    const ids = await IDB_get(this.keyStore)
+    const idIndex = ids.findIndex(id => id === deviceId)
+    let newIds = []
+    if (idIndex !== -1) {
+      newIds = [...ids.slice(0, idIndex), ...ids.slice(idIndex + 1)]
     }
-    this.devices = { ...this.devices, ...device }
-    console.log('new device store', this.devices)
+
+    if (newIds.length) {
+      await IDB_update(this.keyStore, ids => [...ids, ...newIds])
+    }
+
+    await IDB_del(deviceId)
   }
 
-  setDevices(devices) {
-    this.setDevice(devices.reduce((acc, curr) => ({ ...acc, ...this.prepareDevice(curr)}), {}))
+  async getDevice(id) {
+    return await IDB_get(id)
   }
 
-  prepareDevice(device) {
-    return { [device._id]: device }
+  async getDevices(ids) {
+    return (await IDB_getMany(ids)).filter(device => !!device)
   }
 
-  clearDevices() {
-    this.devices = {}
-    this.storeDevices()
-  }
+  async clearDevices() {
+    let keysToDelete = []
+    for (const key of this.devices.keys()) {
+      keysToDelete = [...keysToDelete, key]
+    }
 
-  loadDevices() {
-    this.devices = JSON.parse(localStorage.getItem(this.storageKey))
-  }
-
-  storeDevices() {
-    localStorage.setItem(this.storageKey, JSON.stringify(this.devices))
+    await IDB_delMany(keysToDelete)
+    await IDB_update(this.keyStore, () => [])
   }
 }
+
 
 const deviceStore = new DeviceStore()
 
