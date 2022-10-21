60adabd00ebf58462eca86c44b56f5c076075607
Andrew Wanex <awanex2705@gmail.com>
2022-10-21 12:24:27 -0700

    refactor: update device store to use new storage service

--- src/services/Device/Store/DeviceStore.js
+++ src/services/Device/Store/DeviceStore.js
@@ -1,75 +1,27 @@
-import {
-  get     as IDB_get,
-  getMany as IDB_getMany,
-  set     as IDB_set,
-  update  as IDB_update,
-  del     as IDB_del,
-  delMany as IDB_delMany
-} from 'idb-keyval'
+import storageService from '../../Storage/Storage'
+import { DEVICES_STORE_NAME } from '../../../shared/constants/db-store-names'
 
 
 class DeviceStore {
   constructor() {
     if (DeviceStore._instance) return DeviceStore._instance
     DeviceStore._instance = this
-    this.keyStore = 'device-ids'
-    this.init()
-  }
-
-  async init() {
-    const keys = await IDB_get(this.keyStore)
-    if (!keys) await IDB_set(this.keyStore, [])
-  }
-
-  async setDevice(device) {
-    const entry = await IDB_get(device._id)
-    if (!entry) {
-      await IDB_update(this.keyStore, ids => {
-        if (!ids.includes(device._id)) return [...ids, device._id]
-        return ids
-      })
-      return IDB_set(device._id, device)
-    }
-    return IDB_update(device._id, () => device)
-  }
-
-  async setDevices(devices) {
-    return await Promise.allSettled(devices.map(async device => {
-      return this.setDevice(device)
-    }))
-  }
-
-  async deleteDevice(deviceId) {
-    const ids = await IDB_get(this.keyStore)
-    const idIndex = ids.findIndex(id => id === deviceId)
-    let newIds = []
-    if (idIndex !== -1) {
-      newIds = [...ids.slice(0, idIndex), ...ids.slice(idIndex + 1)]
-    }
-
-    if (newIds.length) {
-      await IDB_update(this.keyStore, ids => [...ids, ...newIds])
-    }
-
-    await IDB_del(deviceId)
   }
 
   async getDevice(id) {
-    return await IDB_get(id)
+    return await storageService.get(DEVICES_STORE_NAME, id)
   }
 
   async getDevices(ids) {
-    return (await IDB_getMany(ids)).filter(device => !!device)
+    return (await storageService.getMany(DEVICES_STORE_NAME, ids)).filter(device => !!device)
   }
 
-  async clearDevices() {
-    let keysToDelete = []
-    for (const key of this.devices.keys()) {
-      keysToDelete = [...keysToDelete, key]
-    }
+  async setDevice(device) {
+    await storageService.set(DEVICES_STORE_NAME, device)
+  }
 
-    await IDB_delMany(keysToDelete)
-    await IDB_update(this.keyStore, () => [])
+  async setDevices(devices) {
+    await storageService.setMany(DEVICES_STORE_NAME, devices)
   }
 }
 
