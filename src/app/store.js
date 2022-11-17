6e7725749b38d29f853fb1d97b485ca8c7d66acd
Andrew Wanex <awanex2705@gmail.com>
2022-11-16 16:17:53 -0800

    feature: add redux store

---
+++ src/app/store.js
@@ -0,0 +1,52 @@
+import { configureStore } from '@reduxjs/toolkit'
+import thunkMiddleware from 'redux-thunk'
+
+import beveragesReducer  from '../services/beverage/store/beverage.slice'
+import containersReducer from '../services/container/store/container.slice'
+import devicesReducer    from '../services/device/store/device.slice'
+import draftsReducer     from '../services/draft/store/draft.slice'
+import tokenReducer      from '../services/token/store/token.slice'
+import userReducer       from '../services/user/store/user.slice'
+
+import { localStorageMiddleware } from './middleware/local-storage'
+import { clearAllStoresMiddleware } from './middleware/clear-all'
+import { preLoadDataMiddleware } from './middleware/pre-load'
+
+import { persistState, clearPersistentState } from './persist/persist'
+
+import { ON_INIT } from '../shared/constants/shared-event-names'
+
+
+const store = configureStore({
+  reducer: {
+    beverages : beveragesReducer,
+    containers: containersReducer,
+    devices   : devicesReducer,
+    drafts    : draftsReducer,
+    token     : tokenReducer,
+    user      : userReducer
+  },
+  middleware: getDefaultMiddleware => (
+    getDefaultMiddleware().concat([
+      thunkMiddleware,
+      localStorageMiddleware,
+      preLoadDataMiddleware,
+      clearAllStoresMiddleware
+    ])
+  )
+})
+
+store.subscribe(() => {
+  const state = store.getState()
+  if (state.user.remember) {
+    for (const key in state) {
+      persistState(key, state)
+    }
+  } else {
+    clearPersistentState()
+  }
+})
+
+store.dispatch({ type: ON_INIT })
+
+export default store
