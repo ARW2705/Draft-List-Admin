da8a34cc857f40c27e4d644f7b7062a5c20d43ec
Andrew Wanex <awanex2705@gmail.com>
2022-11-16 17:25:49 -0800

    refactor: integrate redux

--- src/components/DeviceComponents/Device/Device.jsx
+++ src/components/DeviceComponents/Device/Device.jsx
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-import Button from '../../Common/Button/Button'
-import Locale from '../../Locale/Locale'
+import { selectDevice } from '../../../services/device/store/device.slice'
+
+import Button  from '../../Common/Button/Button'
 import Divider from '../../Common/Divider/Divider'
-import Image from '../../Common/Image/Image'
+import Image   from '../../Common/Image/Image'
+import Locale  from '../../Locale/Locale'
 
 import './Device.css'
 
 
-function Device({ device, onClick: handleOnClick }) {
+function Device({ deviceId }) {
+  const device = useSelector(state => selectDevice(state, deviceId))
   const { name, title, imageURL, locale, draftList } = device
   const [ activeDraftCount, setActiveDraftCount ] = useState(null)
 
   useEffect(() => {
     setActiveDraftCount(draftList.reduce((acc, curr) => acc + curr.isActive ? 1 : 0, 0))
   }, [draftList])
+
+  const location = useLocation()
+  const navigate = useNavigate()
+  const handleOnClick = action => {
+    if (action === 'edit') {
+      navigate(`${location.pathname}/form`, { state: { device } })
+    } else if (action === 'draft') {
+      // TODO: navigate to drafts - draftlist will prioritize drafts belonging to this device
+      console.log('nav to drafts of device', device)
+    } else {
+      throw new Error(`Invalid click action: ${action}`)
+    }
+  }
 
   return (
     <article className='device'>
@@ -25,14 +43,14 @@ function Device({ device, onClick: handleOnClick }) {
       />
       <div className='right-side'>
         <div className='device-identifier'>
-          <span>{ title ?? name }</span>
+          <span>{ title ? title : name }</span>
           <Locale { ...locale } />
         </div>
         <div className='device-button-container'>
           <Button
             text='Edit Device'
             customClass='device-edit-button'
-            onClick={ () => handleOnClick('edit', device) }
+            onClick={ () => handleOnClick('edit') }
             isFlat={ true }
           />
           <Divider color='secondary-dark' />
@@ -40,7 +58,7 @@ function Device({ device, onClick: handleOnClick }) {
             text={ `${activeDraftCount} Active Drafts`}
             customClass='draft-count-button'
             ariaLabel='nav to drafts by device'
-            onClick={ () => handleOnClick('draft', device) }
+            onClick={ () => handleOnClick('draft') }
             isFlat={ true }
           />
         </div>
