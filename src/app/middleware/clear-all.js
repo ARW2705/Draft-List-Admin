d8f2f6e2520023424601cff11e7c297fff589870
Andrew Wanex <awanex2705@gmail.com>
2022-11-16 16:18:48 -0800

    feature: add redux clear all event middleware

---
+++ src/app/middleware/clear-all.js
@@ -0,0 +1,20 @@
+import { CLEAR_ALL } from '../../shared/constants/shared-event-names'
+import { clear as clearUser } from '../../services/user/store/user.slice'
+import { clear as clearToken } from '../../services/token/store/token.slice'
+import { clear as clearDevices } from '../../services/device/store/device.slice'
+import { clear as clearBeverages } from '../../services/beverage/store/beverage.slice'
+import { clear as clearDrafts } from '../../services/draft/store/draft.slice'
+
+
+export const clearAllStoresMiddleware = store => next => action => {
+  if (action.type === CLEAR_ALL) {
+    console.log('clearing all stores')
+    next(clearUser())
+    next(clearToken())
+    next(clearDevices())
+    next(clearBeverages())
+    next(clearDrafts())
+  }
+
+  return next(action)
+}
