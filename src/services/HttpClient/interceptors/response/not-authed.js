dfe60ec395375504f17378aa1205f3b4b954d977
Andrew Wanex <awanex2705@gmail.com>
2022-11-16 16:58:20 -0800

    chore: remove removeToken call for the time being

--- src/services/HttpClient/interceptors/response/not-authed.js
+++ src/services/HttpClient/interceptors/response/not-authed.js
@@ -1,13 +1,10 @@
-import { removeToken } from '../../../token/token'
-
 function handleUnauthorizedResponse(axiosRef) {
   axiosRef.interceptors.response.use(
     response => response,
     async error => {
       const { response } = error
       if (response.status === 401) {
-        removeToken()
-        // Handle token expired
+        // Handle token error
         return Promise.resolve()
       }
       return Promise.reject(error)
