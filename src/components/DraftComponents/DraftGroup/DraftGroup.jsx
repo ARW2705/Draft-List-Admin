4ceea84b0ea6c6d88259acb98fe0d9459f16cb4d
Andrew Wanex <awanex2705@gmail.com>
2022-11-16 17:17:22 -0800

    refactor: udpate draft group to match changes in draft list and draft components

--- src/components/DraftComponents/DraftGroup/DraftGroup.jsx
+++ src/components/DraftComponents/DraftGroup/DraftGroup.jsx
@@ -1,48 +1,29 @@
-import React, { useCallback, useEffect, useState } from 'react'
-
-import { archiveDraft } from '../../../services/Device/Device'
-import { getBeverageById } from '../../../services/Beverage/Beverage'
-import { getDraftListByIds } from '../../../services/Draft/Draft'
+import React from 'react'
 
 import Draft from '../Draft/Draft'
 
 import './DraftGroup.css'
 
 
-function DraftGroup({ deviceId, deviceName, drafts: initialDrafts }) {
-  const [ draftList, setDraftList ] = useState(initialDrafts)
-  const [ draftComponents, setDraftComponents ] = useState([])
-
-  const removeDraft = useCallback(async draftId => {
-    const updatedDraftList = await archiveDraft(deviceId, draftId)
-    const { drafts: populatedDrafts } = await getDraftListByIds(updatedDraftList)
-    setDraftList(populatedDrafts)
-  }, [deviceId, setDraftList])
-
-  useEffect(() => {
-    async function init() {
-      if (!draftList.length) return setDraftComponents(<p className='empty-list'>Device has no active drafts</p>)
-
-      const populatedBeverages = await Promise.all(draftList.map(draft => getBeverageById(draft.beverage)))
-      setDraftComponents(draftList.map((draft, index) => (
-        <Draft
-          key={ draft._id }
-          deviceId={ deviceId }
-          draftId={ draft._id }
-          container={ draft.container }
-          beverage={ populatedBeverages[index] }
-          removeDraft={ () => removeDraft(draft._id) }
-        />
-      )))
-    }
-    init()
-  }, [draftList, deviceId, setDraftComponents, removeDraft])
+function DraftGroup({ deviceId, deviceName, draftIds }) {
+  let components
+  if (!draftIds.length) {
+    components = <p className='empty-list'>Device has no active drafts</p>
+  } else {
+    components = draftIds.map(draftId => (
+      <Draft
+        key={ draftId }
+        deviceId={ deviceId }
+        draftId={ draftId }
+      />
+    ))
+  }
 
   return (
     <div className='draft-group-container'>
       <h2 className='draft-device-name'>{ deviceName }</h2>
       <div className='draft-group'>
-        { draftComponents }
+        { components }
       </div>
     </div>
   )
