a02812df965b6a057b6d23cf0d4737b4835fa762
Andrew Wanex <awanex2705@gmail.com>
2022-12-07 15:55:36 -0800

    feature: add toast feedback on beverage add and archive events

--- src/components/BeverageComponents/BeverageList/BeverageList.jsx
+++ src/components/BeverageComponents/BeverageList/BeverageList.jsx
@@ -1,26 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState, useRef } from 'react'
 import { useSelector } from 'react-redux'
 
-import { selectActiveBeverageIds, selectBeverageQueryId } from '../../../services/beverage/store/beverage.selector'
+import { selectBeverage, selectActiveBeverageIds, selectBeverageQueryId } from '../../../services/beverage/store/beverage.selector'
 
 import Beverage from '../../BeverageComponents/Beverage/Beverage'
+import Toast    from '../../Common/Toast/Toast'
 
-import shallowCompare from '../../../shared/utilities/shallow-compare'
+import findUniqueInArrays from '../../../shared/utilities/find-unique'
+import shallowCompare     from '../../../shared/utilities/shallow-compare'
 
 import './BeverageList.css'
 
 
 function BeverageList({ listConfig }) {
   const [ components, setComponents ] = useState([])
-  
+  const [ toast, setToast ] = useState(null)
+
   const { searchType, searchTerm } = listConfig
   const selector = (searchType && searchTerm)
     ? state => selectBeverageQueryId(state, searchType, searchTerm)
     : selectActiveBeverageIds
   const beverageIds = useSelector(selector, shallowCompare)
+  const previousBeverageIds = useRef(beverageIds)
+  const beverageIdsDiff = beverageIds.length - previousBeverageIds.current.length
+  const newBeverage = useSelector(state => selectBeverage(state, findUniqueInArrays(beverageIds, previousBeverageIds.current)[0]))
+
+  const setToastFeedback = useCallback(message => {
+    const toastTimeoutDuration = 2 * 1000 // 2 seconds
+    setToast(message)
+    setTimeout(() => {
+      setToast(null)
+    }, toastTimeoutDuration)
+  }, [])
 
   useEffect(() => {
-    if (!beverageIds?.length) {
+    if (beverageIdsDiff > 0) {
+      setToastFeedback(`Added ${newBeverage.title || newBeverage.name}`)
+    } else if (beverageIdsDiff < 0) {
+      setToastFeedback('Archived Beverage')
+    }
+
+    if (!beverageIds.length) {
       setComponents(<p className='empty-list'>Nothing here...</p>)
     } else {
       setComponents(beverageIds.map(id => (
@@ -31,10 +51,13 @@ function BeverageList({ listConfig }) {
         />
       )))
     }
-  }, [beverageIds])
+
+    previousBeverageIds.current = beverageIds
+  }, [beverageIds, beverageIdsDiff, newBeverage, setToastFeedback])
 
   return (
     <div className='beverage-list'>
+      { toast && <Toast message={ toast } /> }
       { components }
     </div>
   )
