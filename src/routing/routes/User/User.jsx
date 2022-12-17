fd2883b74937cf0dcdde690b3d9600d2f41b081e
Andrew Wanex <awanex2705@gmail.com>
2022-12-16 19:35:23 -0800

    refactor: change structure to work with transition group

--- src/routing/routes/User/User.jsx
+++ src/routing/routes/User/User.jsx
@@ -6,24 +6,26 @@ import LoginForm from '../../../components/Forms/Login/Login'
 import Profile from '../../../components/Profile/Profile'
 import LoginSignupButtons from '../../../components/LoginSignupButtons/LoginSignupButtons'
 
+import './User.css'
+
 
 function User() {
   return (
-    <main className='route user fade-in'>
+    <div className='route user-router'>
       <Routes>
-        <Route path='/signup' element={ <SignupForm /> } />
-        <Route path='/login' element={ <LoginForm /> } />
         <Route
           path='/'
           element={
-            <>
+            <div className='user-container'>
               <Profile />
               <LoginSignupButtons />
-            </>
+            </div>
           }
         />
+        <Route path='/signup' element={ <SignupForm /> } />
+        <Route path='/login'  element={ <LoginForm />  } />
       </Routes>
-    </main>
+    </div>
   )
 }
 
