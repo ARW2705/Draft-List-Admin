a8faa9b21d8ebe7f044107dbb0e28f3a70d18681
Andrew Wanex <awanex2705@gmail.com>
2022-12-20 11:17:39 -0800

    refactor: move new draft button to draft route component

--- src/components/DraftComponents/DraftList/DraftList.jsx
+++ src/components/DraftComponents/DraftList/DraftList.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import { selectActiveDrafts } from '../../../services/draft/store/draft.selector'
 
-import Button     from '../../Common/Button/Button'
 import DraftGroup from '../DraftGroup/DraftGroup'
 
 import './DraftList.css'
@@ -12,8 +10,6 @@ import './DraftList.css'
 
 function DraftList() {
   const draftCollection = useSelector(selectActiveDrafts)
-  const location = useLocation()
-  const navigate = useNavigate()
 
   let components = []
   for (const key in draftCollection) {
@@ -31,12 +27,6 @@ function DraftList() {
 
   return (
     <div className='draft-list-container'>
-      <Button
-        text='Add New Draft'
-        name='add-draft'
-        onClick={ () => navigate(`${location.pathname}/form`) }
-        customClass='new-draft-button'
-      />
       { components }
     </div>
   )
--- src/routing/routes/Drafts/Drafts.jsx
+++ src/routing/routes/Drafts/Drafts.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
+import Button    from '../../../components/Common/Button/Button'
 import DraftList from '../../../components/DraftComponents/DraftList/DraftList'
 import DraftForm from '../../../components/DraftComponents/DraftForm/DraftForm'
 
@@ -8,6 +10,8 @@ import './Drafts.css'
 
 
 function Drafts() {
+  const navigate = useNavigate()
+
   return (
     <div className='route drafts-router'>
       <Routes>
@@ -15,6 +19,12 @@ function Drafts() {
           path='/'
           element={
             <div className='drafts-container'>
+              <Button
+                text='Add New Draft'
+                name='add-draft'
+                onClick={ () => navigate('form') }
+                customClass='new-draft-button'
+              />
               <DraftList />
             </div>
           }
