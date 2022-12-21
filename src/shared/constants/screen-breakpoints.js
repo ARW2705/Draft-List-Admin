44aab0a7eac5b09a29332c9714eb418bce781947
Andrew Wanex <awanex2705@gmail.com>
2022-12-20 16:41:56 -0800

    feature: add screen breakpoint definitions

---
+++ src/shared/constants/screen-breakpoints.js
@@ -0,0 +1,20 @@
+const smallScreenBounds = {
+  min: 0,
+  max: 599
+}
+
+const midScreenBounds = {
+  min: 600,
+  max: 1024
+}
+
+const largeScreenBounds = {
+  min: 1025,
+  max: 1800
+}
+
+export const screenBreakPoints = {
+  sm: smallScreenBounds,
+  md: midScreenBounds,
+  lg: largeScreenBounds
+}
