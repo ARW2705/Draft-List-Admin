1bf301d72a4df4342563879823673b7d7f7c1009
Andrew Wanex <awanex2705@gmail.com>
2023-01-16 12:33:11 -0800

    refactor: remove unnecessary form error from form select; only possible validation is 'required' which is better suited for form level validation instead of field level; simplify component now that state management is simpler

--- src/components/Common/Form/Select/Select.jsx
+++ src/components/Common/Form/Select/Select.jsx
@@ -1,90 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-import FormError from '../FormError/FormError'
 import DropDown from '../../DropDown/DropDown'
 
-import { validate } from '../../../../shared/validators/validators'
-import hyphenify    from '../../../../shared/utilities/hyphenify'
-import toTitleCase  from '../../../../shared/utilities/title-case'
+import toTitleCase from '../../../../shared/utilities/title-case'
 
 import './Select.css'
 
 
 function FormSelect(props) {
-  const { config, validators, handleOnChange, customClass } = props
+  const { config, handleOnChange } = props
   const { name, value, label, selectOptions } = config
   
-  let initLabel = toTitleCase(label || name)
+  const optionLabels = selectOptions.map(option => option.label)
+  let title = toTitleCase(label || name)
   if (value) {
-    const option = selectOptions.find(option => {
-      if (typeof option === 'object') {
-        return JSON.stringify(option.value) === JSON.stringify(value)
-      }
-      return option.value === value
+    const option = selectOptions.find(selectOption => {
+      return JSON.stringify(selectOption.value) === JSON.stringify(value)
     })
-    if (option) initLabel = option.label
-  }
-
-  const [ attrs, setAttrs ] = useState({
-    name,
-    handleOnChange,
-    value,
-    label: initLabel,
-    id: hyphenify(`form-select-${name}`),
-    customClass: customClass || '',
-    options: selectOptions.map(option => option.label)
-  })
-  const [ touchStatus, setTouchStatus ] = useState({
-    focus: false,
-    touched: !!value,
-    pristine: !value
-  })
-  const [ errorState, setErrorState ] = useState({
-    errors: {},
-    show: false
-  })
-
-  useEffect(() => {
-    if (touchStatus.touched) {
-      setErrorState(prevProps => ({ ...prevProps, show: true }))
-    }
-  }, [touchStatus])
-
-  const checkValidity = (name, value) => {
-    const errors = validate(value, validators)
-    setErrorState(() => ({ errors, show: touchStatus.touched }))
-    attrs.handleOnChange(name, value, errors)
+    if (option) title = option.label
   }
 
   const handleSelect = selectionLabel => {
     const { value } = selectOptions.find(option => option.label === selectionLabel)
-    checkValidity(name, value)
-    setAttrs(prevProps => ({ ...prevProps, value }))
+    handleOnChange(name, value)
   }
 
   return (
-    <div className={`form-select-container ${attrs.customClass}`}>
+    <div className='form-select-container'>
       <DropDown
-        title={ attrs.label }
-        items={ attrs.options }
+        title={ title }
+        items={ optionLabels }
         customClass='form-select-dropdown'
         onSelect={ handleSelect }
       />
-      {
-        errorState.show
-        && (
-          <FormError
-            name={ attrs.name }
-            errors={ errorState.errors }
-          />
-        )
-      }
     </div>
   )
 }
 
-function compare(prevProps, nextProps) {
-  return JSON.stringify(prevProps.config) === JSON.stringify(nextProps.config)
-}
 
-export default React.memo(FormSelect, compare)
+export default React.memo(FormSelect)
