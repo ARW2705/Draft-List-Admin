e6f9e108297f79089c472bfc683a9e87719b7265
Andrew Wanex <awanex2705@gmail.com>
2023-01-16 17:01:16 -0800

    refactor: remove list component generation from useeffect and place directly in element splitting button content into its own factory function

--- src/components/Common/Form/List/List.jsx
+++ src/components/Common/Form/List/List.jsx
@@ -1,19 +1,17 @@
-import React, { Fragment, useCallback, useEffect, useState } from 'react'
+import React, { Fragment, useState } from 'react'
 
-import Spinner    from '../../Loaders/Spinner/Spinner'
-import Button     from '../../Button/Button'
+import Button from '../../Button/Button'
 
-import hyphenify    from '../../../../shared/utilities/hyphenify'
-import toTitleCase  from '../../../../shared/utilities/title-case'
+import hyphenify   from '../../../../shared/utilities/hyphenify'
+import toTitleCase from '../../../../shared/utilities/title-case'
 
 import './List.css'
 
 
 function FormList(props) {
-  const { config, handleOnChange, customClass = '' } = props
+  const { config, handleOnChange } = props
   const { name, value, label, list, displayKeys } = config
 
-  const [ listComponent, setListComponent ] = useState([])
   const [ selectedIndex, setSelectedIndex ] = useState(value ? 0 : -1)
   const [ attrs, setAttrs ] = useState({
     value,
@@ -21,62 +19,52 @@ function FormList(props) {
     id: hyphenify(`form-input-${name}`)
   })
 
-  const handleClick = useCallback(index => {
+  const buildButtonContent = item => {
+    return displayKeys.map((key, index) => {
+      let content = <span key={ key }>{ toTitleCase(item[key]) }</span>
+      if (index !== displayKeys.length - 1) {
+        content = (
+          <Fragment key={ key }>
+            { content }
+            <span className='content-separator'>•</span>
+          </Fragment>
+        )
+      }
+
+      return content
+    })
+  }
+
+  const handleClick = index => {
     const value = list[index]
     setAttrs(prevProps => ({ ...prevProps, value }))
     setSelectedIndex(index)
     handleOnChange(name, value, {})
-  }, [name, list, handleOnChange])
-
-  useEffect(() => {
-    if (!list.length) setListComponent([<div>List is empty...</div>])
-
-    setListComponent(
-      <ul className='form-list'>
-        {
-          list.map((item, index) => (
-            <li key={ index } className='form-list-item'>
-              <Button
-                content={ <>
-                  {
-                    displayKeys.map((key, index) => {
-                      let content = <span key={ key }>{ toTitleCase(item[key]) }</span>
-                      if (index !== displayKeys.length - 1) {
-                        content = (
-                          <Fragment key={ key }>
-                            { content }
-                            <span className='display-separator'>•</span>
-                          </Fragment>
-                        )
-                      }
-                      return content
-                    })
-                  }
-                </> }
-                onClick ={ () => handleClick(index) }
-                customClass={`list-button ${selectedIndex === index ? 'active' : ''}`}
-                name={`select ${name} button`}
-                ariaLabel={`select ${name}`}
-                isFlat={ true }
-              />
-            </li>
-          ))
-        }
-      </ul>
-    )
-  }, [attrs, name, list, selectedIndex, displayKeys, handleClick, setListComponent])
+  }
 
   return (
-    <div className={`form-list-container ${customClass}`}>
+    <div className='form-list-container'>
       <h3>{ attrs.label }</h3>
-      { listComponent ?? <Spinner /> }
+      <ul className='form-list'>
+      {
+        list.map((item, index) => (
+          <li key={ index } className='form-list-item'>
+            <Button
+              content={ buildButtonContent(item) }
+              onClick ={ () => handleClick(index) }
+              customClass={`list-button ${selectedIndex === index ? 'active' : ''}`}
+              name={`select ${name} button`}
+              ariaLabel={`select ${name}`}
+              isFlat={ true }
+            />
+          </li>
+        ))
+      }
+      </ul>
     </div>
   )
 }
 
-function compare(prevProps, nextProps) {
-  return JSON.stringify(prevProps.config) === JSON.stringify(nextProps.config)
-}
 
-export default React.memo(FormList, compare)
+export default React.memo(FormList)
 
